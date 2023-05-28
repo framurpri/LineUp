a668d5785d357e6a6889a9f6e56a1ffd9b4abe78
migueegomez7 <miguelangelgomezx2@gmail.com>
2023-05-28 16:11:12 +0200

    Añadidos iconos en la barra de abajo y empezado el Settings

--- BasicStructPage.jsx
+++ BasicStructPage.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { StyleSheet, View , Text} from 'react-native';
-import DownBar from './DownBar.jsx'
+import DownBar from './DownBar.jsx';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { NativeRouter, Routes, Route, Link } from 'react-router-native';
 const Basic = () => {
     return (
         <View>
         <DownBar>
-            <Text style={styles.buttonText}>Opción 1</Text>
-            <Text style={styles.buttonText}>Opción 2</Text>
-            <Text style={styles.buttonText}>Opción 3</Text>
-            <Text style={styles.buttonText}>Opción 3</Text>
+            <Icon name="film" size={25} color="#900" />
+            <Icon name="group" size={25} color="#900" />
+            <Icon name="user" size={25} color="#900" />
+            <Link to={{ pathname: '/settings'}}>
+                 <Icon name="cog" size={25} color="#900" />
+            </Link>
         </DownBar>
         </View>
     );
 };
+
+const Login = () => (
+    <Routes>
+      <Route path="/" element={<Basic />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
+);
 
 const styles = StyleSheet.create({
     buttonText: {
